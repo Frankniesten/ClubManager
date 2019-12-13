d14b6376a0a44cc5889be561eaf905c8096ceae0
Frank Niesten <f.niesten@fontys.nl>
2019-12-13 01:09:14 +0100

    New styling
    - New base styling
    - New Dashboard look & Feel

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,25 +1,29 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\WidgetTotalMembers;
-use App\Service\WidgetBirthdays;
-use App\Service\WidgetJubilee;
 use App\Service\WidgetEvents;
+use App\Service\WidgetProfile;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/", name="dashboard")
      */
-    public function index(WidgetEvents $events)
+    public function index(WidgetEvents $events, WidgetProfile $profile)
     {
+	    $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+	    $user = $this->getUser();
+	    
+	    $id=$user->getPerson();
+	    
 	    $events = $events->WidgetEvents();
+	    $profile = $profile->WidgetProfile($id);
 	    
         return $this->render('dashboard/dashboard.html.twig', [
-            'events' =>$events
+            'events' =>$events,
+            'profile' =>$profile
         ]);
     }
 }
