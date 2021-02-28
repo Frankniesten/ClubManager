add5101a5470940c9440f0576454557311382f3b
Frank Niesten <f.niesten@fontys.nl>
2021-02-28 13:26:00 +0100

    Donation statistics

--- src/Service/WidgetDonations.php
+++ src/Service/WidgetDonations.php
@@ -10,32 +10,41 @@ class WidgetDonations extends AbstractController
 {
 	public function WidgetDonations($id)
     {
-
         $donations = array();
-
 
         $em = $this->getDoctrine()->getManager();
         $data = $em->getRepository(Donation::class)->findByFund($id);
 
-
         foreach ($data as $key => $value)
         {
-
+            //Get date and map in the right dateformat.
             $date = $value->getDonationDate();
-            $date = $date->format('d-m-Y');
+            $date = $date->format('Y-m-d');
 
+            //Get amount.
+            $amount = (float) $value->getAmount();
 
-
-
-            $donations[$key]['date'] = $date;
-            $donations[$key]['amount'] = $value->getAmount();
-
-            //Haal de gegevens op.
-            //Check of de datum al bestaat.
-            //Indien ja tel de waarde erbij op
-            //Indien nee creeer een nieuwe waarde.
+            //Check if date exists in donations array.
+            if (array_key_exists($date, $donations))
+            {
+                $oldAmount = $donations[$date];
+                $newAmount = $oldAmount + $amount;
+                $donations[$date] = $newAmount;
+            }
+            else
+            {
+                $donations[$date] = $amount;
+            }
         }
 
-		return $donations;
+        $convert = array();
+        $i = 0;
+        foreach ($donations as $key => $value)
+        {
+            $convert[$i]['date'] = $key;
+            $convert[$i]['amount'] = $value;
+            $i++;
+        }
+		return $convert;
 	}
 }
